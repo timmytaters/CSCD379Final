d19aab33a93f72b3a99c906aed37d4306fe8ad58
Timothy N <tnelson32@ewu.edu>
2024-06-06 11:49:52 -0700

    DB setup

--- Wordle.Api/Wordle.Api.Tests/obj/Debug/net8.0/Wordle.Api.Tests.AssemblyInfo.cs
+++ Wordle.Api/Wordle.Api.Tests/obj/Debug/net8.0/Wordle.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wordle.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b681fa66f2870a2ec289fc8aab38d18e789172be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc135aa5a1cec8c42808b5de050c93258d5c3fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wordle.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wordle.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
